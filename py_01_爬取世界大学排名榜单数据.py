ed21d286151910cfe82193886c3d9595e5c55628
HuangKunHong <huangkunhom@outlook.com>
2023-06-07 01:13:07 +0800

    Changes

---
+++ py_01_爬取世界大学排名榜单数据.py
@@ -0,0 +1,17 @@
+# 课程链接：https://www.bilibili.com/video/BV1r8411o7EH
+
+"""
+案例实现流程：
+    思路分析
+    https://www.qschina.cn/sites/default/files/qs-rankings-data/cn/2174878.txt?_=1686070956374
+    二,代码实现
+    1,发送请求
+    2.获取数据
+    3.解析数据
+    4.保存数据
+"""
+import requests
+
+url = 'https://www.qschina.cn/sites/default/files/qs-rankings-data/cn/2174878.txt?_=1686070956374'
+# 1. 发送请求
+requests.get()
