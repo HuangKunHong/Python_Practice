543818017440ce3a20e43cf87f81c471abdc279a
HuangKunHong <huangkunhom@outlook.com>
2023-06-07 01:38:04 +0800

    Changes

--- py_01_爬取世界大学排名榜单数据.py
+++ py_01_爬取世界大学排名榜单数据.py
@@ -13,5 +13,11 @@
 import requests
 
 url = 'https://www.qschina.cn/sites/default/files/qs-rankings-data/cn/2174878.txt?_=1686070956374'
+
 # 1. 发送请求
-requests.get()
+response = requests.get(url=url)
+# 2. 获取数据
+# .text: 获取文本数据
+# .json(): 获取数据以后，将数据转换为json类型
+# .content：获取二进制数据
+print(response.text)
